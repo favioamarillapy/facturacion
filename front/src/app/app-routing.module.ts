7cb320c91af63dd57847773e82f7fc568cce6bd9
Matias Báez <matiasbaez2512@gmail.com>
2020-06-18 17:44:05 -0400

    Se agrego el guard para proteger las rutas

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -5,14 +5,16 @@ import { FacturacionComponent } from './pages/facturacion/facturacion.component'
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ClienteComponent } from './pages/cliente/cliente.component';
 import { TimbradoComponent } from './pages/timbrado/timbrado.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'facturacion', component: FacturacionComponent },
-  { path: 'cliente', component: ClienteComponent },
-  { path: 'timbrado', component: TimbradoComponent }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'facturacion', component: FacturacionComponent, canActivate: [AuthGuard] },
+  { path: 'cliente', component: ClienteComponent, canActivate: [AuthGuard] },
+  { path: 'timbrado', component: TimbradoComponent, canActivate: [AuthGuard] },
+  { path: '**', redirectTo: 'dashboard'}
 ];
 
 
